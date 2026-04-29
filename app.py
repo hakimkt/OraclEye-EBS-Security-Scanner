0b8a480c1c44f97bfe46cbf0ca9184e089534d5d
hakimkt <hakimkt@gmail.com>
2026-04-30 00:16:15 +0300

    Update app to run on port 80
    Change the application port from 5000 to 80.

--- app.py
+++ app.py
@@ -126,10 +126,6 @@ def list_scans():
     ])
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, port=5000)
+    app.run(debug=True, threaded=True, port=80)
 
-import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
